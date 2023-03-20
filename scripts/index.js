d48cb96c0bf514c3e398a6e6ddf3ab6bc34ce1dc
Alan Shieh <piratejing41@gmail.com>
2023-03-19 17:36:06 -0700

    sprint 4 stasge 4

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,47 @@ let initialCards = [
   },
 ];
 
-let editButton = document.querySelector(".profile__button-edit");
-let closeButton = document.querySelector(".modal__button-close");
-let modal = document.querySelector(".modal");
+const editButton = document.querySelector(".profile__button-edit");
+const closeButton = document.querySelector(".modal__button-close");
+const modal = document.querySelector(".modal");
+const profileName = document.querySelector(".profile__name");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const inputName = document.querySelector("#name");
+const inputSubtitle = document.querySelector("#description");
+const cardsEl = document.querySelector(".cards");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+
+function closeModal() {
+  modal.classList.remove("modal-open");
+}
+
+function formSave(e) {
+  e.preventDefault();
+  profileName.textContent = inputName.value;
+  profileSubtitle.textContent = inputSubtitle.value;
+  closeModal();
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardNameEl = cardElement.querySelector(".card__name");
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  cardNameEl.textContent = data.name;
+  return cardElement;
+}
 
 editButton.addEventListener("click", function () {
   modal.classList.add("modal-open");
+  inputName.value = profileName.textContent;
+  inputSubtitle.value = profileSubtitle.textContent;
 });
 
-closeButton.addEventListener("click", function () {
-  modal.classList.remove("modal-open");
+closeButton.addEventListener("click", closeModal);
+modal.addEventListener("submit", formSave);
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardsEl.append(cardElement);
 });
